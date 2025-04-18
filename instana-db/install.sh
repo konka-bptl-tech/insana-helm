efb0aab24cdb3c7d59d85293470e88ec53308fed
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-04-18 12:08:37 +0530

    update the script

--- instana-db/install.sh
+++ instana-db/install.sh
@@ -4,25 +4,24 @@ install_or_upgrade() {
     RELEASE_NAME=$1
     CHART_PATH=$2
     VALUES_FILE=$3
-    NAMESPACE=$4
 
     if helm list -n "$NAMESPACE" | grep -q "^$RELEASE_NAME"; then
         echo "Upgrading $RELEASE_NAME..."
-        helm upgrade "$RELEASE_NAME" "$CHART_PATH" -f "$VALUES_FILE" -n "$NAMESPACE"
+        helm upgrade "$RELEASE_NAME" "$CHART_PATH" -f "$VALUES_FILE"
     else
         echo "Installing $RELEASE_NAME..."
-        helm install "$RELEASE_NAME" "$CHART_PATH" -f "$VALUES_FILE" -n "$NAMESPACE"
+        helm install "$RELEASE_NAME" "$CHART_PATH" -f "$VALUES_FILE"
     fi
 }
 
 NAMESPACE="instana"
 
-install_or_upgrade "mongo" "." "dev/mongo.yaml" "$NAMESPACE"
+install_or_upgrade "mongo" "." "dev/mongo.yaml" 
 sleep 10
-install_or_upgrade "mysql" "." "dev/mysql.yaml" "$NAMESPACE"
+install_or_upgrade "mysql" "." "dev/mysql.yaml"
 sleep 10
-install_or_upgrade "rabbitmq" "." "dev/rabbitmq.yaml" "$NAMESPACE"
+install_or_upgrade "rabbitmq" "." "dev/rabbitmq.yaml"
 sleep 10
-install_or_upgrade "redis" "." "dev/redis.yaml" "$NAMESPACE"
+install_or_upgrade "redis" "." "dev/redis.yaml"
 
     
